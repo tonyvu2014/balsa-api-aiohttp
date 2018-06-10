5cfc4dbb0269b6adc28e979ebbef0ba3ba6756cf
Tony Vu <tonyvu2008@gmail.com>
2018-06-10 15:25:28 +1000

    Remove refresh endpoint

--- app.py
+++ app.py
@@ -21,29 +21,14 @@ async def get_feeds(request):
         content_type='application/json'
     )
 
-async def refresh(request):
-    data = await request.json()
-    terms = data.get('terms', [])
-    limit = data.get('limit', None)
-    feeds = list(read_all_news_feeds(terms))
-    feeds.sort(key=lambda x: x.published_date, reverse=True)
-    results = feeds[:limit] if limit else feeds
-    return web.Response(
-        status=200, 
-        body=json.dumps([{'title': feed.title, 'link': feed.link, 'published_date': feed.published_date.strftime(DATE_TIME_FORMAT)} for feed in feeds], indent=4).encode('UTF-8'),
-        content_type='application/json'
-    )
-
 async def web_app():
     app = web.Application()
     app.router.add_get('/', index)
     app.router.add_post('/api/feeds', get_feeds)
-    app.router.add_post('/refresh', refresh)
     return app
 
 if __name__=='__main__':
     app = web.Application()
     app.router.add_get('/', index)
     app.router.add_post('/api/feeds', get_feeds)
-    app.router.add_post('/refresh', refresh)
     web.run_app(app)
